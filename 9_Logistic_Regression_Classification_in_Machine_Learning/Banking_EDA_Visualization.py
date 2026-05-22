b9543039cb7f778f4481de3571785b156caf7586
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2026-05-22 18:10:48 +0500

    Update Banking_EDA_Visualization.py

--- 9_Logistic_Regression_Classification_in_Machine_Learning/Banking_EDA_Visualization.py
+++ 9_Logistic_Regression_Classification_in_Machine_Learning/Banking_EDA_Visualization.py
@@ -1,48 +1,62 @@
-table.div(table.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
 import matplotlib.pyplot as plt
-plt.title("Stacked Bar Chart of Education vs Purchase")
 import numpy as np
-plt.xlabel("Education")
+import pandas as pd
+import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-plt.show()
 from sklearn import preprocessing
-pd.crosstab(data.day_of_week, data.y).plot(kind="bar")
-plt.title("Purchase Frequency for Day of Week")
+
 url = "https://raw.githubusercontent.com/madmashup/targeted-marketing-predictive-engine/master/banking.csv"
-plt.xlabel("Day of Week")
-plt.ylabel("Frequency of Purchase")
 data = pd.read_csv(url, header=0)
 data = data.dropna()
-pd.crosstab(data.month, data.y).plot(kind="bar")
+
 data["education"] = np.where(data["education"] == "basic.9y", "Basic", data["education"])
-plt.title("Purchase Frequency for Month")
 data["education"] = np.where(data["education"] == "basic.6y", "Basic", data["education"])
-plt.xlabel("Month")
 data["education"] = np.where(data["education"] == "basic.4y", "Basic", data["education"])
+
+pd.crosstab(data.day_of_week, data.y).plot(kind="bar")
+plt.title("Purchase Frequency for Day of Week")
+plt.xlabel("Day of Week")
 plt.ylabel("Frequency of Purchase")
-pd.crosstab(data.job, data.y).plot(kind="bar")
 plt.show()
+
+pd.crosstab(data.month, data.y).plot(kind="bar")
+plt.title("Purchase Frequency for Month")
+plt.xlabel("Month")
+plt.ylabel("Frequency of Purchase")
+plt.show()
+
+pd.crosstab(data.job, data.y).plot(kind="bar")
 plt.title("Purchase Frequency for Job Title")
 plt.xlabel("Job")
-data.age.hist()
 plt.ylabel("Frequency of Purchase")
-plt.title("Histogram of Age")
 plt.savefig("purchase_fre_job")
+plt.show()
+
+data.age.hist()
+plt.title("Histogram of Age")
 plt.xlabel("Age")
-plt.show()
 plt.ylabel("Frequency")
-table = pd.crosstab(data.marital, data.y)
 plt.savefig("hist_age")
-table.div(table.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
 plt.show()
-table = pd.crosstab(data.poutcome, data.y).plot(kind="bar")
+
+table = pd.crosstab(data.marital, data.y)
+table.div(table.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
 plt.title("Stacked Bar Chart of Marital Status vs Purchase")
 plt.xlabel("Marital Status")
 plt.ylabel("Proportion of Customers")
-plt.xlabel("Poutcome")
 plt.savefig("marital_vs_pur_stack")
+plt.show()
+
+pd.crosstab(data.poutcome, data.y).plot(kind="bar")
+plt.title("Purchase Frequency for Poutcome")
+plt.xlabel("Poutcome")
 plt.ylabel("Frequency of Purchase")
 plt.show()
+
 table = pd.crosstab(data.education, data.y)
+table.div(table.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.title("Stacked Bar Chart of Education vs Purchase")
+plt.xlabel("Education")
+plt.ylabel("Proportion of Customers")
 plt.show()
